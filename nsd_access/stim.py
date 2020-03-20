a84f7e16043b97cf8e32e26c3236772aad95fb86
Tomas Knapen <tknapen@gmail.com>
2020-03-20 19:54:30 +0100

    stimulus can now get get its annotations and category

--- nsd_access/stim.py
+++ nsd_access/stim.py
@@ -1,9 +1,13 @@
-from .nsda import NSDAccess
+import numpy as np
+from pandas.io.json import json_normalize
 from pycocotools.coco import COCO
+from .nsda import NSDAccess
 
 
 class NSDStimulus(object):
     """ NSDStimulus is a class that encapsulates NSD stimulus information.
+
+    it uses NSDAccess's coco annotation information objects to get image metadata
     """
 
     def __init__(self, image_index, nsda):
@@ -23,3 +27,18 @@ class NSDStimulus(object):
             coco_annot_IDs = coco.getAnnIds([self.coco_image_ID])
             setattr(self, 'coco_annot_'+info_type,
                     coco.loadAnns(coco_annot_IDs))
+
+    def get_coco_category(self):
+        """get_coco_category internalizes the coco_categories for this image.
+        """
+        self.cat_ids = self.coco_annot_instances.getCatIds()
+        self.categories = json_normalize(
+            self.coco_annot_instances.loadCats(self.cat_ids))
+        self.coco_categories = []
+        for cat_id in self.cat_ids:
+            this_img_list = self.coco_annot_instances.getImgIds(catIds=[
+                                                                cat_id])
+            if self.coco_image_id in this_img_list:
+                this_cat = np.asarray(
+                    categories[categories['id'] == cat_id]['name'])[0]
+                self.coco_categories.append(this_cat)
